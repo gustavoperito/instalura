767af5ce6348606f06063a6c2dfc7bfc035f8b07
Gustavo Alano Perito <gustavoaperito@hotmail.com>
2018-10-19 00:37:41 -0300

    surf1

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import { AppRegistry } from 'react-native';
+import Navigator from './src';
 import {name as appName} from './app.json';
 
+const App = Navigator.StackNavigator; // Descomente para usar o StackNavigator.
+// const App = Navigator.TabNavigator; // Descomente para usar o TabNavigator.
+// const App = Navigator.DrawerNavigator; // Descomente para usar o DrawerNavigator.
+
 AppRegistry.registerComponent(appName, () => App);
+console.disableYellowBox = true;
